5ffb8d09fc5bc8f1001aaeac3cdd452da67ec628
kaowan <127403483+kaowan@users.noreply.github.com>
2023-09-01 11:57:22 -0300

    01/09

---
+++ JavaNew3108-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,17 @@
+function tocaSom (idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
+}
+
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+
+while (contador < ListaDeTeclas.length) {
+    ListaDeTeclas[contador].onclick = tocaSom;
+
+    contador = contador +1;
+
+    console.log(contador)
+}
