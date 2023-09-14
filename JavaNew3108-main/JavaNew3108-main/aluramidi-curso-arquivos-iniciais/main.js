153fe00ad8b13a395ef27aeef53fad411cf50e27
kaowan <127403483+kaowan@users.noreply.github.com>
2023-09-14 11:47:15 -0300

    14/09
    14/09 - Aula nada produtiva

---
+++ JavaNew3108-main/JavaNew3108-main/aluramidi-curso-arquivos-iniciais/main.js
@@ -0,0 +1,22 @@
+function tocaSom (idElementoAudio) {
+
+    document.querySelector(idElementoAudio).play();
+}
+const ListaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < ListaDeTeclas.length) {
+
+    const tecla = ListaDeTeclas[contador];
+   const instrumento = tecla.classList[1];
+
+   const idAudio = `som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    contador = contador +1;
+
+}
